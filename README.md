26852e2c61b02be8cf7caea4327237601f35c798
Arashk <arashkyoosha@gmail.com>
2022-04-04 19:55:04 -0400

    Changes to README file

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # hw-4-code-quiz
+
+Homework 4 of GT bootcamp is the JavaScript Quiz
+
+In this homework, we created a timed JS quiz which deducts point for each wrong answer.
+
+Live demo:
+
+<https://arashkyoosha.github.io/hw-4-code-quiz/>
--- assets/script.js
+++ assets/script.js
@@ -176,28 +176,4 @@ function allDone() {
   createSubmit.textContent = "Submit";
 
   questionsDiv.appendChild(createSubmit);
-
-  // Event listener to capture initials and local storage for initials and score
-  createSubmit.addEventListener("click", function () {
-    var initials = createInput.value;
-
-    if (initials === null) {
-      console.log("No value entered!");
-    } else {
-      var finalScore = {
-        initials: initials,
-        score: timeRemaining,
-      };
-      console.log(finalScore);
-      var allScores = localStorage.getItem("allScores");
-      if (allScores === null) {
-        allScores = [];
-      } else {
-        allScores = JSON.parse(allScores);
-      }
-      allScores.push(finalScore);
-      var newScore = JSON.stringify(allScores);
-      localStorage.setItem("allScores", newScore);
-    }
-  });
 }
